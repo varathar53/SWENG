2d13c96c9c5e301cdc46d8b449413df01304c41d
Senthuasa <senthuasa.varatharasan@bf.uzh.ch>
2019-09-20 12:49:50 +0200

    added a method and changed the sysout

--- Bootcamp/src/HelloWorld.java
+++ Bootcamp/src/HelloWorld.java
@@ -2,6 +2,10 @@
 public class HelloWorld {
 	
 	  public static void main(String[] args) {
-	    System.out.println("Hello SWENG!");
+	    System.out.println("The answer to the ultimate question of Life, The Universe, and Everything is: " + computeUltimateAnswer());
 	  }
+
+public static int computeUltimateAnswer() {
+	return 6 * 7;
+}
 	}
